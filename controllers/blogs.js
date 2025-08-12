ee50ff16db61e9932c95ef51042f143d3f265da6
AKBAR U <91365919+urlenbayev@users.noreply.github.com>
2025-08-12 20:29:14 +0300

    13.13

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,7 +1,9 @@
 import express from "express";
 const router = express.Router();
+import { Op } from "sequelize";
 import { User, Blog } from "../models/index.js";
 import { blogFinder, tokenExtractor } from "../middleware/middleware.js";
+
 /**
 |--------------------------------------------------
 GET http://localhost:3001/api/blogs
@@ -29,15 +31,27 @@ Example data
 |--------------------------------------------------
 */
 router.get("/", async (req, res) => {
-  const blogs = await Blog.findAll({
-    include: {
-      model: User,
-    },
-  });
-  if (blogs.length === 0) {
-    return res.status(404).json({ error: "Blogs do not exist" });
+  const { search } = req.query;
+  if (!search) {
+    const blogs = await Blog.findAll({
+      include: {
+        model: User,
+      },
+    });
+    if (blogs.length === 0) {
+      return res.status(404).json({ error: "Blogs do not exist" });
+    }
+    return res.status(200).json(blogs);
+  } else {
+    //Filter by a search word
+    const blogs = await Blog.findAll({
+      where: { title: { [Op.iLike]: `%${search}%` } },
+    });
+    if (blogs.length === 0) {
+      return res.status(404).json({ error: "Blogs do not exist" });
+    }
+    res.status(200).json(blogs);
   }
-  return res.status(200).json(blogs);
 });
 
 /**
